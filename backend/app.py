aa6d424a9066da705252a32329ddb2a12fc53907
23f3001764 <23f3001764@ds.study.iitm.ac.in>
2025-03-29 16:56:39 +0530

    Implemented token-based authentication & RBAC

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager
 from model import db,User
 import os
 from datetime import timedelta
-from api import LoginAPI, SignupAPI, cache
+from api import LoginAPI, SignupAPI, cache, NotifyAPI, UserAPI, SubjectAPI, ChapterAPI, QuizAPI, QuestionAPI, AnswerAPI, ScoreAPI
 
 app = Flask(__name__)
 
@@ -37,6 +37,14 @@ def add_admin():
 
 api.add_resource(SignupAPI, '/api/signup', '/api/signup/<string:username>')
 api.add_resource(LoginAPI, '/api/login')
+api.add_resource(NotifyAPI,  '/api/notify','/api/notify/<int:notify_id>'  ,'/api/notifies/<int:chap_id>')
+api.add_resource(UserAPI, '/api/user')
+api.add_resource(SubjectAPI, '/api/subject', '/api/subject/<int:subject_id>')
+api.add_resource(ChapterAPI, '/api/chapter', '/api/chapter/<string:sname>','/api/chapter/<int:chapter_id>')
+api.add_resource(QuizAPI, '/api/quiz', '/api/quiz/<string:chname>', '/api/quiz/<int:quiz_id>')
+api.add_resource(QuestionAPI, '/api/ques', '/api/ques/<int:quiz_id>', '/api/question/<int:ques_id>')
+api.add_resource(AnswerAPI, '/api/ans', '/api/ans/<int:ans_id>', '/api/answer/<int:ques_id>')
+api.add_resource(ScoreAPI, '/api/score', '/api/score/<int:score_id>' ,'/api/scores/<int:quiz_id>', '/api/scores/<int:quiz_id>/<int:user_id>' , '/api/sco/<int:user_id>')
 
 if __name__=="__main__":
     db.create_all()
